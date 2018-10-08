95c3a4f71d93af6581fc56b930608f1398471b3d
Kristofer Riedman <riedmank@hotmail.com>
2018-10-08 16:28:28 -0700

    file structure setup

---
+++ Challenges/reverseArray/reverseArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

302e39ff68ec4cafa7c90f6931f83b36739b6aa6
Kristofer Riedman <riedmank@hotmail.com>
2018-10-10 08:08:08 -0700

    added picture, comments to code, and stretch goal

---
+++ Challenges/arrayShift/README.md
@@ -0,0 +1,12 @@
+# Insert and shift middle index of array
+
+## Challenge
+Given an array and a value, create a new array with the value inserted into the middle of the array.
+
+## Approach & Efficiency 
+Big O Time = O(n) // Big O Space = O(n)
+- For this particular code challenge, Big O of Time and Big O of Space are both O(n). Big O of Time is O(n) becuase we need to iterate through an array of n number of elements.  Big O of Space is O(n) becuase we need to instatiate a new array of n number of elements in order to insert a new value.
+
+## Solution
+![Solution](../../assets/ArrayShift-Whiteboard-1)
+![Solution](../../assets/ArrayShift-Whiteboard-2)
---
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace arrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays created for testing insert into array: odd and even length array
+            int[] testOne = { 1, 2, 4, 5 };
+            int[] testTwo = { 1, 2, 4 };
+
+            // Test value to insert
+            int test = 3;
+
+            // Array created for testing remove from array: odd and even length array
+            int[] testThree = { 1, 2, 10, 3, 4 };
+            int[] testFour = { 1, 2, 3, 10, 4, 5 };
+
+            // Assigning array results
+            int[] resultOne = ArrayShift(testOne, test);
+            int[] resultTwo = ArrayShift(testTwo, test);
+            int[] resultThree = ArrayRemove(testThree);
+            int[] resultFour = ArrayRemove(testFour);
+
+            // Printing results
+            Console.WriteLine("First array:");
+            foreach (int num in resultOne)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Second array:");
+            foreach (int num in resultTwo)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Remove test One:");
+            foreach (int num in resultThree)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Remove test Two:");
+            foreach (int num in resultFour)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        // Adds user value into the array at the middle index
+        static int[] ArrayShift(int[] arr, int num)
+        {
+            // Create new array
+            int[] newArray = new int[arr.Length + 1];
+            // Find middle value
+            int middle = arr.Length % 2 != 0 ? (arr.Length / 2) + 1 : arr.Length / 2;
+            // Insert old array into new array with new value in the middle index
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (i == middle) newArray[i] = num;
+                else if (i < middle) newArray[i] = arr[i];
+                else newArray[i] = arr[i - 1];
+            }
+            return newArray;
+        }
+
+        // Removes the value at the middle index
+        static int[] ArrayRemove(int[] arr)
+        {
+            // Create new array
+            int[] newArray = new int[arr.Length - 1];
+            // Find middle value
+            int middle = newArray.Length % 2 != 0 ? (newArray.Length / 2) + 1 : newArray.Length / 2;
+            // Insert old array into new array with middle value removed
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < middle) newArray[i] = arr[i];
+                else if (i == middle) continue;
+                else newArray[i - 1] = arr[i];
+            }
+            return newArray;
+        }
+    }
+}

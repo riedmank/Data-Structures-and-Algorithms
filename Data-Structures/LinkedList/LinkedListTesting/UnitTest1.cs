111d76b0fa697a3cd49181e760adfaa7c2b7f8d2
Kristofer Riedman <riedmank@hotmail.com>
2018-10-16 09:02:13 -0700

    added 15 passing tests

--- Data-Structures/LinkedList/LinkedListTesting/UnitTest1.cs
+++ Data-Structures/LinkedList/LinkedListTesting/UnitTest1.cs
@@ -44,5 +44,62 @@ namespace LinkedListTesting
 
             Assert.Null(node4.Next);
         }
+
+        [Theory]
+        [InlineData("potato")]
+        [InlineData(22)]
+        [InlineData(false)]
+        public void LLCanFind(object value)
+        {
+            Node node1 = new Node(10);
+            Node node2 = new Node(null);
+            Node node3 = new Node("Dune");
+            Node node4 = new Node(value);
+            LinkedListClass LL = new LinkedListClass(node1);
+
+            LL.Append(node2);
+            LL.Append(node3);
+            LL.Append(node4);
+
+            Assert.Equal(value, LL.Find(value).Value);
+        }
+
+        [Theory]
+        [InlineData("potato")]
+        [InlineData(22)]
+        [InlineData(false)]
+        public void LLCanAddAfter(object value)
+        {
+            Node node1 = new Node(10);
+            Node node2 = new Node(null);
+            Node node3 = new Node("Dune");
+            Node node4 = new Node(value);
+            LinkedListClass LL = new LinkedListClass(node1);
+
+            LL.Append(node2);
+            LL.Append(node3);
+            LL.AddAfter(node4, node2);
+
+            Assert.Equal(node4, node2.Next);
+        }
+
+        [Theory]
+        [InlineData("potato")]
+        [InlineData(22)]
+        [InlineData(false)]
+        public void LLCanAddBefore(object value)
+        {
+            Node node1 = new Node(10);
+            Node node2 = new Node(null);
+            Node node3 = new Node("Dune");
+            Node node4 = new Node(value);
+            LinkedListClass LL = new LinkedListClass(node1);
+
+            LL.Append(node2);
+            LL.Append(node3);
+            LL.AddBefore(node4, node3);
+
+            Assert.Equal(node3, node4.Next);
+        }
     }
 }

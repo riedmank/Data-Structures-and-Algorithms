3395b74fdbce762a0776ef9a4298bcabf0822ce4
Kristofer Riedman <riedmank@hotmail.com>
2018-10-16 19:09:07 -0700

    added readme

--- Challenges/FindKthElement/FindKthElement/Program.cs
+++ Challenges/FindKthElement/FindKthElement/Program.cs
@@ -3,16 +3,9 @@ using LinkedList.Classes;
 
 namespace FindKthElement
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            
-            
-            NodeFinderAtK(12);
-        }
-
-        public static object NodeFinderAtK(int k)
+        public static void Main(string[] args)
         {
             Node node1 = new Node("node1");
             Node node2 = new Node("node2");
@@ -21,7 +14,18 @@ namespace FindKthElement
             Node node5 = new Node("node5");
 
             LL ll = new LL(node1);
-            
+
+            NodeFinderAtK(1, ll);
+        }
+
+        /// <summary>
+        /// Finds the Kth node from end of a Linked List
+        /// </summary>
+        /// <param name="k">The target value</param>
+        /// <param name="ll">A Linked List</param>
+        /// <returns>Returns the value at the searched for node</returns>
+        public static object NodeFinderAtK(int k, LL ll)
+        {
             int counter = 0;
             ll.Current = ll.Head;
             while (ll.Current != null)
@@ -30,6 +34,18 @@ namespace FindKthElement
                 ll.Current = ll.Current.Next;
             }
             ll.Current = ll.Head;
+            try
+            {
+                if (k > counter)
+                {
+                    throw new Exception("Value is outside Linked List");
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            
             for (int i = 0; i < counter - k; i++)
             {
                 ll.Current = ll.Current.Next;

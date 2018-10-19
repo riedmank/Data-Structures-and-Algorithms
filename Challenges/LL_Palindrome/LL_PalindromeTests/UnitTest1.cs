8e3db54d44178c758c1eb72bc5e9dc05fb7440f8
Kristofer Riedman <riedmank@hotmail.com>
2018-10-19 13:45:48 -0700

    method written and working

---
+++ Challenges/LL_Palindrome/LL_PalindromeTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using static LL_Palindrome.Program;
+using LinkedList.Classes;
+
+namespace LL_PalindromeTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EvenPalindrome()
+        {
+
+        }
+
+        OddPalindrome()
+
+        NotAPalindrome
+    }
+}

822f2fa176a85a0693c3dbc854abd553e2104b89
Kristofer Riedman <riedmank@hotmail.com>
2018-10-22 13:51:05 -0700

    added implementation for queue class and methods

--- Challenges/QueuesWithStacks/QueuesWithStacks/Classes/QWS.cs
+++ Challenges/QueuesWithStacks/QueuesWithStacks/Classes/QWS.cs
@@ -7,6 +7,32 @@ namespace QueuesWithStacks.Classes
 {
     public class QWS
     {
+        Stack Stack1 = new Stack(null);
+        Stack Stack2 = new Stack(null);
 
+        public QWS(Stack StackA, Stack StackB)
+        {
+            Stack1 = StackA;
+            Stack2 = StackB;
+        }
+
+        public void Enqueue(Node value)
+        {
+            Stack1.Push(value);
+        }
+
+        public Node Dequeue()
+        {
+            while (Stack1.Top.Next != null)
+            {
+                Stack2.Push(Stack1.Pop());
+            }
+            Node temp = Stack1.Pop();
+            while (Stack2.Top != null)
+            {
+                Stack1.Push(Stack2.Pop());
+            }
+            return temp;
+        }
     }
 }

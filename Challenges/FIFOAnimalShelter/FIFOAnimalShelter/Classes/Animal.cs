ed432d947532384ddcf1046f95f4660fbe4c985a
Kristofer Riedman <riedmank@hotmail.com>
2018-10-23 13:35:34 -0700

    initial file setup and created animal class

---
+++ Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    class Animal
+    {
+        public string AnimalType { get; set; }
+
+        public Animal(string value)
+        {
+            AnimalType = value;
+        }
+    }
+}

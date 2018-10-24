2b8cca7b337970369396690c67bdac5df631a938
Kristofer Riedman <riedmank@hotmail.com>
2018-10-24 14:57:40 -0700

    refactored code to use my implemented stack

--- Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using StacksAndQueues.Classes;
 
 namespace MultiBracketValidation
 {
@@ -32,63 +32,44 @@ namespace MultiBracketValidation
         /// <returns>Returns true if there are pairs and false if there aren't</returns>
         public static bool MultiBracketValidationMethod(string value)
         {
-            Stack<string> BCP = new Stack<string>();
+            Stack BCP = new Stack(null);
+            Node bracket = new Node("]");
+            Node curly = new Node("}");
+            Node paran = new Node(")");
 
             for (int i = 0; i < value.Length; i++)
             {
                 if (value[i].ToString() == "[")
-                {
-                    BCP.Push("]");
-                }
+                    BCP.Push(bracket);
                 if (value[i].ToString() == "{")
-                {
-                    BCP.Push("}");
-                }
+                    BCP.Push(curly);
                 if (value[i].ToString() == "(")
-                {
-                    BCP.Push(")");
-                }
+                    BCP.Push(paran);
 
                 try
                 {
-                    if (value[i].ToString() == "]" && BCP.Peek() != "]")
-                    {
+                    if (value[i].ToString() == "]" && (string)BCP.Peek().Value != "]")
                         return false;
-                    }
-                    else if (value[i].ToString() == "]" && BCP.Peek() == "]")
-                    {
+                    else if (value[i].ToString() == "]" && (string)BCP.Peek().Value == "]")
                         BCP.Pop();
-                    }
-                    if (value[i].ToString() == "}" && BCP.Peek() != "}")
-                    {
+                    if (value[i].ToString() == "}" && (string)BCP.Peek().Value != "}")
                         return false;
-                    }
-                    else if (value[i].ToString() == "}" && BCP.Peek() == "}")
-                    {
+                    else if (value[i].ToString() == "}" && (string)BCP.Peek().Value == "}")
                         BCP.Pop();
-                    }
-                    if (value[i].ToString() == ")" && BCP.Peek() != ")")
-                    {
+                    if (value[i].ToString() == ")" && (string)BCP.Peek().Value != ")")
                         return false;
-                    }
-                    else if (value[i].ToString() == ")" && BCP.Peek() == ")")
-                    {
+                    else if (value[i].ToString() == ")" && (string)BCP.Peek().Value == ")")
                         BCP.Pop();
-                    }
                 }
                 catch
                 {
                     return false;
                 }
             }
-            if (BCP.Count == 0)
-            {
+            if (BCP.Top == null)
                 return true;
-            }
             else
-            {
                 return false;
-            }
         }
     }
 }

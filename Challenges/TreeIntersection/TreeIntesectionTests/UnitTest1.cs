0742472e80362018c74815571b95aeb1da7ddfd9
Kristofer Riedman <riedmank@hotmail.com>
2018-11-13 13:33:10 -0800

    added image and implemented method

---
+++ Challenges/TreeIntersection/TreeIntesectionTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using static TreeIntersection.Program;
+using Tree.Classes;
+using HashTables.Classes;
+
+namespace TreeIntesectionTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

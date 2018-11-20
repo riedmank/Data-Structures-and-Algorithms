20282cec7d89738d32bd75d34512036746898c59
Kristofer Riedman <riedmank@hotmail.com>
2018-11-19 16:57:12 -0800

    implemented method, added comments, and updated readme

---
+++ Data-Structures/Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Sorting_Algorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Unsorted Array:");
+            int[] arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Sorted Array:");
+            int[] result = InsertionSort(arr);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// Performs insertion sort
+        /// </summary>
+        /// <param name="myArray">Unsorted array</param>
+        /// <returns>Returns a sorted array</returns>
+        public static int[] InsertionSort(int[] myArray)
+        {
+            for (int i = 1; i < myArray.Length; i++)
+            {
+                int temp = myArray[i];
+                int j = i - 1;
+
+                while (j >= 0 && temp < myArray[j])
+                {
+                    myArray[j + 1] = myArray[j];
+                    j--;
+                }
+                myArray[j + 1] = temp;
+            }
+            return myArray;
+        }
+    }
+}

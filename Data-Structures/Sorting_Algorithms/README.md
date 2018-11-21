26e43ea588ef28fb7f5ae9fca5dd2933a7fed5db
Kristofer Riedman <riedmank@hotmail.com>
2018-11-21 08:49:04 -0800

    implemented method and updated readme

--- Data-Structures/Sorting_Algorithms/README.md
+++ Data-Structures/Sorting_Algorithms/README.md
@@ -1,7 +1,12 @@
 # Sorting Algorithms
 ## Insertion Sort
-### Given an unsorted array, sort the array using Insertion Sort
 ### Approach and Efficiency
 Big O Time: O(n^2) // Big O Space: O(1)
 - Time is O(n^2) because we have to iterate over the length of the array for every element of the array
+- Space is O(1) because we are making all of the swaps in place
+
+## Quick Sort
+### Approach and Efficiency
+Big O Time: O(n^2) // Big O Space: O(1)
+- Time is O(n^2) because we have to iterate over the array multiple times
 - Space is O(1) because we are making all of the swaps in place
--- Data-Structures/Sorting_Algorithms/Sorting_Algorithms/Program.cs
+++ Data-Structures/Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -7,17 +7,33 @@ namespace Sorting_Algorithms
         public static void Main(string[] args)
         {
             Console.WriteLine("Unsorted Array:");
-            int[] arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-            foreach (int item in arr)
+            int[] arr1 = new int[] { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            foreach (int item in arr1)
             {
-                Console.WriteLine(item);
+                Console.Write($"{item} ");
             }
-            Console.WriteLine("Sorted Array:");
-            int[] result = InsertionSort(arr);
-            foreach (int item in result)
+            Console.WriteLine();
+            Console.WriteLine("Sort Array using Insertion Sort:");
+            int[] insertionResult = InsertionSort(arr1);
+            foreach (int item in insertionResult)
             {
-                Console.WriteLine(item);
+                Console.Write($"{item} ");
             }
+            Console.WriteLine();
+            int[] arr2 = new int[] { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Console.WriteLine("Unsorted Array:");
+            foreach (int item in arr2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sort Array using Quick Sort:");
+            QuickSort(arr2, 0, arr2.Length - 1);
+            foreach (int item in arr2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
         }
 
         /// <summary>
@@ -40,6 +56,62 @@ namespace Sorting_Algorithms
                 myArray[j + 1] = temp;
             }
             return myArray;
+        }
+
+
+        /// <summary>
+        /// Quick sort algorithm
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int position = Partition(arr, left, right);
+
+                QuickSort(arr, left, position - 1);
+
+                QuickSort(arr, position + 1, right);
+            }
+        }
+
+        /// <summary>
+        /// Partitions the specified arr.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>Returns lowest value + 1</returns>
+        public static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int low = left - 1;
+
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i] <= pivot)
+                {
+                    low++;
+                    Swap(arr, i, low);
+                }
+            }
+            Swap(arr, right, low + 1);
+            return low + 1;
+        }
+
+        /// <summary>
+        /// Swaps the specified arr.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <param name="i">The i.</param>
+        /// <param name="low">The low.</param>
+        public static void Swap(int[] arr, int i, int low)
+        {
+            int temp = arr[i];
+            arr[i] = arr[low];
+            arr[low] = temp;
         }
     }
 }
